4aaba4b8e27c9ef7248bf316b23ea21a65adb3c5
Carlos Gonzalez <cagl18@gmail.com>
2019-07-25 14:05:37 -0400

    added search result spinner and pagination functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global state of the app 
 * - Search object
@@ -21,12 +21,14 @@ const controlSearch = async () => {
         //3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         //4) Search for recipes
         await state.search.getResults(); //awaiting for the promise result to return
 
         //5) Render results on UI
-       searchView.renderResults(state.search.result);
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
 }
 
@@ -35,3 +37,11 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn){
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result,goToPage);
+    }
+}); //using event delegation to change pages (displaying results and appropiate btns)
