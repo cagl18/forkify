a682e8f6143f44a840bd5861a1c76b15282c9145
Carlos Gonzalez <cagl18@gmail.com>
2019-07-29 16:55:06 -0400

    added functionality to change recipe servings

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -27,7 +27,7 @@ const formatCount = count => {
 const createIngredient = ingredient => `
     <li class="recipe__item">
         <svg class="recipe__icon">
-            <use href="dist/img/icons.svg#icon-check"></use>
+            <use href="img/icons.svg#icon-check"></use>
         </svg>
         <div class="recipe__count">${formatCount(ingredient.count)}</div>
         <div class="recipe__ingredient">
@@ -49,27 +49,27 @@ export const renderRecipe = recipe => {
     <div class="recipe__details">
         <div class="recipe__info">
             <svg class="recipe__info-icon">
-                <use href="dist/img/icons.svg#icon-stopwatch"></use>
+                <use href="img/icons.svg#icon-stopwatch"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${recipe.time}</span>
             <span class="recipe__info-text"> minutes</span>
         </div>
         <div class="recipe__info">
             <svg class="recipe__info-icon">
-                <use href="dist/img/icons.svg#icon-man"></use>
+                <use href="img/icons.svg#icon-man"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text"> servings</span>
 
             <div class="recipe__info-buttons">
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-decrease">
                     <svg>
-                        <use href="dist/img/icons.svg#icon-circle-with-minus"></use>
+                        <use href="img/icons.svg#icon-circle-with-minus"></use>
                     </svg>
                 </button>
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-increase">
                     <svg>
-                        <use href="dist/img/icons.svg#icon-circle-with-plus"></use>
+                        <use href="img/icons.svg#icon-circle-with-plus"></use>
                     </svg>
                 </button>
             </div>
@@ -77,7 +77,7 @@ export const renderRecipe = recipe => {
         </div>
         <button class="recipe__love">
             <svg class="header__likes">
-                <use href="dist/img/icons.svg#icon-heart-outlined"></use>
+                <use href="img/icons.svg#icon-heart-outlined"></use>
             </svg>
         </button>
     </div>
@@ -93,7 +93,7 @@ export const renderRecipe = recipe => {
 
         <button class="btn-small recipe__btn">
             <svg class="search__icon">
-                <use href="dist/img/icons.svg#icon-shopping-cart"></use>
+                <use href="img/icons.svg#icon-shopping-cart"></use>
             </svg>
             <span>Add to shopping list</span>
         </button>
@@ -108,7 +108,7 @@ export const renderRecipe = recipe => {
         <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
             <span>Directions</span>
             <svg class="search__icon">
-                <use href="dist/img/icons.svg#icon-triangle-right"></use>
+                <use href="img/icons.svg#icon-triangle-right"></use>
             </svg>
 
         </a>
@@ -118,3 +118,15 @@ export const renderRecipe = recipe => {
     elements.recipe.insertAdjacentHTML('afterbegin',markup);
 
 };
+
+export const updateServingsIngredients = recipe => {
+    // Update servings
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+    // Update ingredients
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i) => {
+        el.textContent = formatCount(recipe.ingredients[i].count);
+    });
+
+}
